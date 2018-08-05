7dc041f675d68d28bf95a5a2836a6cca1ecdc4d3
manfered <35757736+manfered@users.noreply.github.com>
2018-08-05 13:54:50 -0600

    Add project files.

---
+++ SymmetricEncryptionTripleDES/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SymmetricEncryptionTripleDES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var trippleDES = new TripleDESEncryption();
+
+            var key = trippleDES.GenerateRandomeNumbers(24);
+
+            var iv = trippleDES.GenerateRandomeNumbers(8);
+            const string message = "Message to Encrypt";
+
+            var encrypted = trippleDES.Encrypt(Encoding.UTF8.GetBytes(message), key, iv);
+            var decrypted = trippleDES.Decrypt(encrypted, key, iv);
+
+            var decryptedMessage = Encoding.UTF8.GetString(decrypted);
+
+            Console.WriteLine("Triple DES Encryption");
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine($"Message text = {message}");
+            Console.WriteLine($"Encrypted Text = {Convert.ToBase64String(encrypted)}");
+            Console.WriteLine($"Decrypted text = {decryptedMessage}");
+        }
+    }
+}
---
+++ SymmetricEncryptionTripleDES/TripleDESEncryption.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace SymmetricEncryptionTripleDES
+{
+    public class TripleDESEncryption
+    {
+        public byte[] GenerateRandomeNumbers(int length)
+        {
+            using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[length];
+                randomNumberGenerator.GetBytes(randomNumber);
+
+                return randomNumber;
+            }
+        }
+
+        public byte[] Encrypt(byte[] toBeEncrypted, byte[] key, byte[] iv)
+        {
+            using (var tripleDES = new TripleDESCryptoServiceProvider())
+            {
+                tripleDES.Mode = CipherMode.CBC;
+                tripleDES.Padding = PaddingMode.PKCS7;
+
+                tripleDES.Key = key;
+                tripleDES.IV = iv;
+
+                using (var memoryStream = new MemoryStream())
+                {
+                    var cryptoStream = new CryptoStream(memoryStream, tripleDES.CreateEncryptor(), CryptoStreamMode.Write);
+
+                    cryptoStream.Write(toBeEncrypted, 0, toBeEncrypted.Length);
+                    cryptoStream.FlushFinalBlock();
+
+                    return memoryStream.ToArray();
+                }
+            }
+        }
+
+        public byte[] Decrypt(byte[] toBeDecrypted, byte[] key, byte[] iv)
+        {
+            using (var tripleDES = new TripleDESCryptoServiceProvider())
+            {
+                tripleDES.Mode = CipherMode.CBC;
+                tripleDES.Padding = PaddingMode.PKCS7;
+
+                tripleDES.Key = key;
+                tripleDES.IV = iv;
+
+                using (var memoryStream = new MemoryStream())
+                {
+                    var cryptoStream = new CryptoStream(memoryStream, tripleDES.CreateDecryptor(), CryptoStreamMode.Write);
+
+                    cryptoStream.Write(toBeDecrypted, 0, toBeDecrypted.Length);
+                    cryptoStream.FlushFinalBlock();
+
+                    return memoryStream.ToArray();
+                }
+
+            }
+        }
+    }
+}
